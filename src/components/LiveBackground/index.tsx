9be1223404533522fbd8206372c6ae58265bd566
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2023-02-25 03:28:37 +0330

    Increased live bg shapes size

--- src/components/LiveBackground/index.tsx
+++ src/components/LiveBackground/index.tsx
@@ -2,9 +2,9 @@ export default function LiveBackground() {
   // to reduced CPU usage, animations are disabled
   return (
     <div className="fixed top-0 left-0 h-[100vh] w-[100vw] opacity-30">
-      <span className="absolute -bottom-10 -right-10 h-52 w-52 rounded-full bg-pink-500" />
-      <span className="absolute bottom-36 right-24 h-72 w-72 rounded-full bg-primary" />
-      <span className="absolute bottom-0 right-72 h-52 w-64 rounded-full bg-blue-500" />
+      <span className="absolute -bottom-10 -right-10 h-80 w-80 rounded-full bg-pink-500" />
+      <span className="absolute bottom-44 right-36 h-96 w-96 rounded-full bg-primary" />
+      <span className="absolute -bottom-10 right-80 h-64 w-64 rounded-full bg-blue-500" />
     </div>
   );
 }
