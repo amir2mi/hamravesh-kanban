11bdcc7f3067bb302bd757dbc59f00d29532ca59
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2023-02-25 03:09:51 +0330

    Added live background component and removed unused animations

--- src/components/LiveBackground/index.tsx
+++ src/components/LiveBackground/index.tsx
@@ -1,3 +1,9 @@
 export default function LiveBackground() {
-  return null;
+  return (
+    <div className="fixed top-0 left-0 h-[100vh] w-[100vw] opacity-30">
+      <span className="absolute -bottom-10 -right-10 h-52 w-52 rounded-full bg-pink-500" />
+      <span className="absolute bottom-36 right-24 h-72 w-72 rounded-full bg-primary" />
+      <span className="absolute bottom-0 right-72 h-52 w-64 rounded-full bg-blue-500" />
+    </div>
+  );
 }
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -8,6 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { setModalData } from "@store/modals";
 import { TodoItemProps } from "@store/todos";
 import KanbanColumns from "./columns";
+import LiveBackground from "@components/LiveBackground";
 
 export default function MainPage() {
   const dispatch = useDispatch();
@@ -23,24 +24,27 @@ export default function MainPage() {
   };
 
   return (
-    <Layout className="min-h-[100vh] overflow-x-hidden">
-      <Header />
-      <div className="container mx-auto mb-4 px-4">
-        <div className="item-center mt-10 flex justify-between">
-          <h2 className="m-0 text-3xl font-bold">لیست تسک‌ها</h2>
-          <Button
-            type="primary"
-            size="large"
-            className="flex items-center"
-            icon={<FontAwesomeIcon icon={faPlus} className="ml-2" />}
-            onClick={handleOnAddTodo}
-          >
-            ایجاد تسک
-          </Button>
+    <>
+      <Layout className="relative z-10 min-h-[100vh] overflow-x-hidden bg-transparent backdrop-blur-3xl">
+        <Header />
+        <div className="container mx-auto mb-4 px-4">
+          <div className="item-center mt-10 flex justify-between">
+            <h2 className="m-0 text-3xl font-bold">لیست تسک‌ها</h2>
+            <Button
+              type="primary"
+              size="large"
+              className="flex items-center"
+              icon={<FontAwesomeIcon icon={faPlus} className="ml-2" />}
+              onClick={handleOnAddTodo}
+            >
+              ایجاد تسک
+            </Button>
+          </div>
+          <KanbanColumns />
+          <KanbanModal onClose={handleOnModalClose} />
         </div>
-        <KanbanColumns />
-        <KanbanModal onClose={handleOnModalClose} />
-      </div>
-    </Layout>
+      </Layout>
+      <LiveBackground />
+    </>
   );
 }
